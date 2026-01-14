e7a6dfd9e05fa28a2f37fce981f377c9fcaa5729
Mohammad Yasin <mohammad.yasin.zak@gmail.com>
2026-01-14 09:56:15 +0800

    -"Create Recipes" button in Recipes page. -Hidden the Create Recipes page for those that are not logged in. -Removed the "Add Recipes" button in NavBar. (Can change back if you want)

---
+++ Recipe_Sharing_Website/Migrations/20260114011315_isprem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Recipe_Sharing_Website.Migrations
+{
+    /// <inheritdoc />
+    public partial class isprem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
